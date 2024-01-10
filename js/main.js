fbbc3b0643a9afcf4aa6b4b0361d3795dfb40a58
bonvi83 <bonvi83@gmail.com>
2024-01-10 17:12:25 +0100

    inserimento dati js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// devo aggiungere variabili scope
+const userName = document.getElementById ("name")
+const userKm = document.getElementById ("km")
+const userAge = document.getElementById ("age-select")
+
+const sendButton = document.getElementById ("invia");
 
 
-
-sendButton.addEventListener ("click"
-    function () {
-
-    }
-
-);
+sendButton.addEventListener ("click", function () {
+    const ;
+});
