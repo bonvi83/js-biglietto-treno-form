8916a7dd6e18da661279540bd1077e52ed07f767
bonvi83 <bonvi83@gmail.com>
2024-01-10 19:00:19 +0100

    non mollo!!!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const userName = document.getElementById ("name")
+const userName = document.getElementById ("name");
 
-const userKm = document.getElementById ("km")
-const userAge = document.getElementById ("age-select")
+const userKm = document.getElementById ("km");
+const userAge = document.getElementById ("age-select");
 
 const sendButton = document.getElementById ("invia");
 
+let valoreEta;
 
 
 
@@ -15,11 +16,11 @@ sendButton.addEventListener ("click", function () {
     const valoreKm = parseInt(userKm.value);
     console.log(valoreKm);
 
-    const valoreEta = parseInt(userAge.value);
-    console.log(userAge);
+    valoreEta = (userAge.value);
+    console.log(valoreEta);
 
 
-    const parzialTicket = userKm * 0.21;
+    const parzialTicket = valoreKm * 0.21;
     console.log(parzialTicket);
 
     let sconto20 = parzialTicket * 0.2;
@@ -28,16 +29,15 @@ sendButton.addEventListener ("click", function () {
     //non so se serva o meno...
     let priceTicket = 0;
 
-    if (age == "under") {
+    if (valoreEta == "under") {
         priceTicket = parzialTicket - sconto20;
-    } else if (age == "over") {
+    } else if (valoreEta == "over") {
         priceTicket = parzialTicket - sconto40;
     } else {
         priceTicket = parzialTicket;
     }
-    
 
     // Comunico all'utente la cifra MA NON HO LA PIU' PALLIDA IDEA DI COME SI FA
-    document.getElementById ("output-costo").innerHTML = priceTicket.toFixed(2) + " €";
+    document.getElementById("output-costo").innerHTML = priceTicket.toFixed(2) + " €";
 
 });
